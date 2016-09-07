ee72a88cca963efc6e521318a538f7ea9ae15446
Andrey Khitryy <andrey.khitryy@gmail.com>
2016-09-07 19:26:10 +0300

    asd

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -24,7 +24,7 @@ def Categories(title):
             url = "http://storage14.swap.sampo.ru/%D0%A1%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D1%8B/%D0%97%D0%B0%D1%80%D1%83%D0%B1%D0%B5%D0%B6%D0%BD%D1%8B%D0%B5/%D0%A2%D0%B5%D0%BE%D1%80%D0%B8%D1%8F%20%D0%91%D0%BE%D0%BB%D1%8C%D1%88%D0%BE%D0%B3%D0%BE%20%D0%92%D0%B7%D1%80%D1%8B%D0%B2%D0%B0%20%28The%20Big%20Bang%20Theory%29/%D0%A1%D0%B5%D0%B7%D0%BE%D0%BD%208/The.Big.Bang.Theory.S08E01.HDTVRip.720p.Kuraj-Bambey.%5Bqqss44%5D.mkv",
             title = "The Big Bang Theory",
             summary = "Some summary",
-            duration = "10:20",
+            duration = 200,
             thumb = Resource.ContentsOfURLWithFallback("http://kino.sampo.ru/resize/poster/9/7309/w210h290.jpg?1463393951")
         ))
 
