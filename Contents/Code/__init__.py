2a48bd05e812655b01a9faf7c54e9a745f545454
Andrey Khitryy <andrey.khitryy@gmail.com>
2016-09-07 18:47:24 +0300

    modity

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,18 @@
+TITLE    = 'Vimeo'
+PREFIX   = '/video/vimeo'
+
+def Start():
+  HTTP.CacheTime = CACHE_1HOUR
+  ObjectContainer.title1 = 'Vimeo'
+
+@handler(PREFIX, TITLE)
+def MainMenu():
+
+  oc = ObjectContainer()
+  # Hard coded the links offered under the Watch pull down list
+  oc.add(DirectoryObject(key=Callback(SectionSort, title='Staff Picks', url=STAFF_PICKS), title='Staff Picks'))
+  oc.add(DirectoryObject(key=Callback(Categories, title='Categories'), title='Categories'))
+  oc.add(DirectoryObject(key=Callback(SubMenu, title='Channels', url=CHANNELS), title='Channels'))
+  oc.add(DirectoryObject(key=Callback(SubMenu, title='Groups', url=GROUPS), title='Groups'))
+  oc.add(InputDirectoryObject(key = Callback(SearchOptions, title="Search"), title = "Search"))
+  return oc
